1e4e59953ee0ae1657043b4564b08b23cde42e96
dkrzyzak <damntheguitars@gmail.com>
2024-07-09 21:21:33 +0200

    create consumer package for bundle tests

---
+++ packages/consumer/src/test.tsx
@@ -0,0 +1,11 @@
+import { Spinner } from '@apocalypse/ui-react';
+
+function Example() {
+	return (
+		<div>
+			<Spinner colorClassName='fill-blue-500' />
+		</div>
+	);
+}
+
+export default Example;
