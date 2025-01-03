cdcd605b27f184a2efbd3457599b705c0177cd05
dkrzyzak <damntheguitars@gmail.com>
2025-01-03 13:38:01 +0100

    use apocalypse/eslint in ui-react

---
+++ packages/ui-react/eslint.config.mjs
@@ -0,0 +1,29 @@
+import { commonConfig, reactConfig } from '@apocalypse/eslint';
+import pluginReactRefresh from 'eslint-plugin-react-refresh';
+import pluginStorybook from 'eslint-plugin-storybook';
+
+
+/** @type {import('eslint').Linter.Config}  */
+export default [
+	{
+		ignores: ['**/dist', '**/.eslintrc.cjs'],
+	},
+	...commonConfig,
+	...reactConfig,
+    ...pluginStorybook.configs['flat/recommended'],
+	{
+		plugins: {
+			'react-refresh': pluginReactRefresh,
+		},
+
+		rules: {
+			'react-refresh/only-export-components': [
+				'warn',
+				{
+					allowConstantExport: true,
+					allowExportNames: ['meta'],
+				},
+			],
+		},
+	},
+];
